360568cf452ca2e218d2d7ec23b25677d22dcb9d
Alok Pandey <alokakv01@gmail.com>
2026-07-08 00:33:20 +0530

    Fix AppRegistryNotReady in asgi.py

--- backend/voicechat/asgi.py
+++ backend/voicechat/asgi.py
@@ -1,13 +1,13 @@
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voicechat.settings")
+
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from signaling.middleware import JWTAuthMiddleware
 import signaling.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voicechat.settings")
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
