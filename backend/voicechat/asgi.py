615f55af37da7f9825d901c5456dd777bb06f260
Alok Pandey <alokakv01@gmail.com>
2026-07-11 09:28:37 +0530

    Remove AllowedHostsOriginValidator to fix WebSocket 403

--- backend/voicechat/asgi.py
+++ backend/voicechat/asgi.py
@@ -5,17 +5,14 @@ from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from signaling.middleware import JWTAuthMiddleware
 import signaling.routing
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            JWTAuthMiddleware(
-                URLRouter(signaling.routing.websocket_urlpatterns)
-            )
+        "websocket": JWTAuthMiddleware(
+            URLRouter(signaling.routing.websocket_urlpatterns)
         ),
     }
 )
